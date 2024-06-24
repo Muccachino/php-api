d3248da8d4bdaf027d5559e1fc6974d609b48022
Muccachino <lucas_staszewski@yahoo.de>
2024-06-24 12:56:36 +0200

    whoops for better error messages

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,1 +1,11 @@
 <?php
+
+namespace Ls\Api\Config;
+
+use Whoops\Handler\JsonResponseHandler as WhoopsJsonResponseHandler;
+use Whoops\Run as WhoopsRun;
+
+
+$whoops = new WhoopsRun();
+$whoops->pushHandler(new WhoopsJsonResponseHandler());
+$whoops->register();
